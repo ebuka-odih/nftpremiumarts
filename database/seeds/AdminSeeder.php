ac30925648759d8bbb919d0e424bf2401685cd03
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2023-01-22 01:27:30 +0100

    commit changes

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -14,7 +14,7 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $admin = User::where('email', '=', 'admin@opennest.io')->first();
+        $admin = User::where('email', '=', 'admin@nftpremiumarts.com')->first();
         if($admin === null){
             DB::table('users')->insert([
                 'name' => 'Admin Admin',
@@ -22,9 +22,9 @@ class AdminSeeder extends Seeder
                 'username' =>'admin',
                 'admin' => 1,
                 'balance' => 500000,
-                'email' => 'admin@opennest.io',
+                'email' => 'admin@nftpremiumarts.com',
                 'email_verified_at' => \Carbon\Carbon::now(),
-                'password' => Hash::make('OPENNEST'),
+                'password' => Hash::make('NFTPREMIUM'),
             ]);
         }
     }
