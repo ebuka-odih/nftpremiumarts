b8c013556d122c26e01779e7a9f9540b3eac93e9
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2023-01-24 13:49:27 +0100

    commit changes

--- resources/views/dashboard/layout/app.blade.php
+++ resources/views/dashboard/layout/app.blade.php
@@ -313,15 +313,15 @@
             <div class="nk-footer nk-footer-fluid">
                 <div class="container-fluid">
                     <div class="nk-footer-wrap">
-                        <div class="nk-footer-copyright"> &copy; 2022 <a href="NFTPremiumarts.io">NFTPremiumarts.io</a>
+                        <div class="nk-footer-copyright"> &copy; 2023 <a href="NFTPremiumarts.io">NFTPremiumarts.io</a>
                         </div>
-                        <div class="nk-footer-links">
-                            <ul class="nav nav-sm">
-                                <li class="nav-item"><a class="nav-link" href="#">Terms</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">Privacy</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">Help</a></li>
-                            </ul>
-                        </div>
+{{--                        <div class="nk-footer-links">--}}
+{{--                            <ul class="nav nav-sm">--}}
+{{--                                <li class="nav-item"><a class="nav-link" href="#">Terms</a></li>--}}
+{{--                                <li class="nav-item"><a class="nav-link" href="#">Privacy</a></li>--}}
+{{--                                <li class="nav-item"><a class="nav-link" href="#">Help</a></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
                     </div>
                 </div>
             </div>
